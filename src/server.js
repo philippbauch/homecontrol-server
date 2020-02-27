fd559dfc29bda0a8f96632e73d43cfcdd747517c
Philipp Bauch <ga53xex@mytum.de>
2020-02-27 11:11:22 +0100

    Remove https server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-const fs = require("fs");
-const https = require("https");
 const app = require("./app");
 const { db } = require("./db");
 const {
@@ -13,13 +11,6 @@ const logger = require("./logger");
 
 const URL = `mongodb://${DB_HOST}:27017`;
 
-const cert = fs.readFileSync("./certs/server.cert");
-const key = fs.readFileSync("./certs/server.key");
-
-const options = { cert, key };
-
-const server = https.createServer(options, app);
-
 db.connect(URL, DB_NAME, {
   auth: {
     user: DB_USER,
@@ -27,7 +18,7 @@ db.connect(URL, DB_NAME, {
   }
 })
   .then(() => {
-    server.listen(PORT, () => {
+    app.listen(PORT, () => {
       logger.info(`Server listening on port ${PORT}...`);
     });
   })
