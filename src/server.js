771e8afbcc4ac107ee9d251adebbd09257c59222
Philipp Bauch <ga53xex@mytum.de>
2020-02-27 18:42:14 +0100

    Create default admin user on server start up

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const app = require("./app");
 const { db } = require("./db");
 const {
@@ -11,17 +12,38 @@ const logger = require("./logger");
 
 const URL = `mongodb://${DB_HOST}:27017`;
 
-db.connect(URL, DB_NAME, {
-  auth: {
-    user: DB_USER,
-    password: DB_PASSWORD
-  }
-})
-  .then(() => {
-    app.listen(PORT, () => {
-      logger.info(`Server listening on port ${PORT}...`);
-    });
-  })
-  .catch(error => {
-    logger.error(error);
+(async function() {
+  await db.connect(URL, DB_NAME, {
+    auth: {
+      user: DB_USER,
+      password: DB_PASSWORD
+    }
   });
+
+  const adminUsers = await db.users.find({ admin: true }).toArray();
+
+  if (!adminUsers.length) {
+    logger.info("No administrative users found.");
+    logger.info("Create default administrative user.");
+
+    const identifier = "admin";
+    const password = "admin";
+    const admin = true;
+    const locked = false;
+
+    const hash = await bcrypt.hash(password, 12);
+
+    const { insertedId } = await db.users.insertOne({
+      identifier,
+      hash,
+      admin,
+      locked
+    });
+
+    logger.info(`Created user with ID ${insertedId}`);
+  }
+
+  app.listen(PORT, () => {
+    logger.info(`Server listening on port ${PORT}...`);
+  });
+})();
