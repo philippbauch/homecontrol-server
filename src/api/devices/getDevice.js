074df6fe30ddb5227d2126ebc0774db36b644e4b
Philipp Bauch <ga53xex@mytum.de>
2020-03-17 22:54:52 +0100

    Add permission check to getDevice route

--- src/api/devices/getDevice.js
+++ src/api/devices/getDevice.js
@@ -1,19 +1,36 @@
 const { db } = require("../../db");
-const { DeviceDoesntExistError } = require("../../errors");
+const { DeviceDoesntExistError, HomeDoesntExistError, PermissionDeniedError, RoomDoesntExistError } = require("../../errors");
 const { wrapAsync } = require("../../utils");
 
 const CONTEXT = "get_device";
 
 const getDevice = wrapAsync(async function(req, res) {
   const { deviceId } = req.params;
+  const { _id: userId } = req.user;
 
   let device = await db.devices.findOne({ _id: deviceId });
 
   if (!device) {
     throw new DeviceDoesntExistError();
-  } else {
-    return res.success(device);
   }
+
+  const room = await db.rooms.findOne({ _id: device.roomId });
+
+  if (!room) {
+    throw new RoomDoesntExistError();
+  }
+
+  const home = await db.homes.findOne({ _id: room.homeId });
+
+  if (!home) {
+    throw new HomeDoesntExistError();
+  }
+
+  if (!home.residents.some(resident => resident._id.equals(userId))) {
+    throw new PermissionDeniedError();
+  }
+
+  res.success(device);
 }, CONTEXT);
 
 module.exports = { CONTEXT, getDevice };
