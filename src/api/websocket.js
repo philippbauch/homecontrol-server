efc2494ef701673978ee8f7986b02495f96df811
Philipp Bauch <ga53xex@mytum.de>
2020-04-16 16:45:58 +0200

    Log incoming WebSocket connection

--- src/api/websocket.js
+++ src/api/websocket.js
@@ -1,5 +1,9 @@
+const logger = require("../logger");
+
 function websocket(req, res) {
   const { _id } = req.user;
+
+  logger.info(`(WebSocket) User connected with ID ${_id}`);
 
   res.websocket(_id);
 }
